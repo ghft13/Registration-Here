cb81b11afa5b021643d2c113a50a737ffd482153
Jayraj <arajjayraj18@gmail.com>
2024-08-20 16:10:10 +0530

    second

--- Config/db.js
+++ Config/db.js
@@ -9,7 +9,7 @@ const connectDB = async () => {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log("MongoDB connected successfully");
+    console.log("MongoDB connected successfully hello");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error.message);
     process.exit(1); // Exit process with failure
