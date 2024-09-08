7f4505d292a9f2383faa7b84cef07ef8652d4920
Jayraj <arajjayraj18@gmail.com>
2024-09-08 11:29:17 +0530

    chnage book

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const flash = require("connect-flash");
 // Import Mongoose models
 const User = require("./models/Usermodel");
 const Appointment = require("./models/Appointmentmodel");
-const { IsLogin } = require("./Middleware/IsLogin");
+const  IsLogin  = require("./Middleware/IsLogin");
 require("dotenv").config();
 
 const {
@@ -57,10 +57,7 @@ app.get("/api/profile",  (req, res) => {
   res.status(200).json({ user: req.user.username, type: "success" });
 });
 
-app.post("/api/auth/book-appointment", IsLogin, (req, res) => {
-  // This route is now protected and accessible only if the user is authenticated
-  BookAppointment(req, res);
-});
+app.post("/api/auth/book-appointment", IsLogin,BookAppointment);
 
 app.get("/api/auth/check-auth", (req, res) => {
   res.status(200).json({ authenticated: true });
